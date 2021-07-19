70f386604603a89fdf4ef80122d15b96893a6f6b
mrinmoy-iitg <56710568+mrinmoy-iitg@users.noreply.github.com>
2021-07-19 11:20:27 +0530

    Major Revision 1

--- lib/misc.py
+++ lib/misc.py
@@ -11,9 +11,12 @@ import os
 import numpy as np
 import pickle
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
 import json
 from sklearn.decomposition import PCA
+from sklearn.feature_extraction.image import PatchExtractor
+import time
+import textgrids
 
 
 
@@ -93,6 +96,7 @@ def create_CV_folds(folder, featName, classes, cv=3):
 
 
 
+
 def get_train_test_files(cv_file_list, cv_file_list_test, numCV, foldNum):
     train_files = {}
     test_files = {}
@@ -112,12 +116,15 @@ def get_train_test_files(cv_file_list, cv_file_list_test, numCV, foldNum):
     
     
     
-def load_data_from_files(classes, folder, featName, files):
+def load_data_from_files(classes, folder, featName, files, patch_size, patch_shift, input_shape):
     label = []
     data = np.empty([])
     for clNum in classes.keys():
         for fl in files[classes[clNum]]:
+            if not os.path.exists(folder + '/' + featName + '/' + classes[clNum] + '/' + fl):
+                continue
             FV = np.load(folder + '/' + featName + '/' + classes[clNum] + '/' + fl, allow_pickle=True)
+            FV = get_feature_patches(FV, patch_size, patch_shift, input_shape)
             if np.size(data)<=1:
                 data = FV
             else:
@@ -130,30 +137,37 @@ def load_data_from_files(classes, folder, featName, files):
 
 
 
+# def getPerformance(PtdLabels, GroundTruths):
+#     # ConfMat = confusion_matrix(y_true=GroundTruths, y_pred=PtdLabels).flatten()
+#     ConfMat = confusion_matrix(y_true=GroundTruths, y_pred=PtdLabels)
+#     Report = classification_report(y_true=GroundTruths, y_pred=PtdLabels, output_dict=True, labels=np.unique(GroundTruths))
+#     fscore = []
+#     Cl = np.unique(GroundTruths)
+#     Report_keys = [key for key in Report.keys()]
+#     for i in Cl:
+#         fscore.append(np.round(Report[Report_keys[int(i)]]['f1-score'], 4))
+#     mean_fscore = np.round(np.mean(fscore), 4)
+#     fscore.append(mean_fscore)
+
+#     return ConfMat, fscore
+
 
 def getPerformance(PtdLabels, GroundTruths):
     ConfMat = confusion_matrix(y_true=GroundTruths, y_pred=PtdLabels)
-    Report = classification_report(y_true=GroundTruths, y_pred=PtdLabels, output_dict=True, labels=np.unique(GroundTruths))
-    fscore = []
-    Cl = [0,1]
-    Report_keys = [key for key in Report.keys()]
-    for i in Cl:
-        try:
-            fscore.append(np.round(Report[Report_keys[int(i)]]['f1-score'], 4))
-        except:
-            fscore.append(0)
+    fscore = f1_score(GroundTruths, PtdLabels, average=None, labels=[0,1]).tolist()
     mean_fscore = np.round(np.mean(fscore), 4)
+    print('mean_fscore: ', mean_fscore)
     fscore.append(mean_fscore)
 
     return ConfMat, fscore
 
 
 
-def print_results(PARAMS, fName_suffix, **kwargs):
+def print_results(PARAMS, folder, fName_suffix, **kwargs):
     if fName_suffix!='':
-        opFile = PARAMS['opDir'] + '/' + PARAMS['clFunc'] + '_performance_' + fName_suffix + '.csv'
+        opFile = folder + '/' + 'Performance_' + fName_suffix + '.csv'
     else:
-        opFile = PARAMS['opDir'] + '/' + PARAMS['clFunc'] + '_performance.csv'
+        opFile = folder + '/' + 'Performance.csv'
         
 
     linecount = 0
@@ -222,3 +236,64 @@ def print_analysis(opFile, results):
         fid.write(values + '\n')
         
     fid.close()
+
+
+
+
+def get_feature_patches(FV, patch_size, patch_shift, input_shape):
+    FV = StandardScaler(copy=False).fit_transform(FV)
+    # FV should be of the shape (nFeatures, nFrames)
+    if any(np.array([9,10,21,22,39])==np.shape(FV)[1]): 
+        FV = FV.T
+    patches = np.empty([])
+
+    if np.shape(FV)[1]<patch_size:
+        # print('Size append: ', np.shape(FV), patch_size)
+        FV1 = FV.copy()
+        while np.shape(FV)[1]<=patch_size:
+            FV = np.append(FV, FV1, axis=1)
+
+    numPatches = int(np.ceil(np.shape(FV)[1]/patch_shift))
+    patches = PatchExtractor(patch_size=(np.shape(FV)[0], patch_size), max_patches=numPatches).transform(np.expand_dims(FV, axis=0))
+
+    patches_mean = np.mean(patches, axis=2)
+    patches_var = np.var(patches, axis=2)
+    patches_mean_var = np.append(patches_mean, patches_var, axis=1)
+    # print('sklearn splitting: ', time.clock()-startTime, np.shape(patches))
+
+    # print('Patches: ', np.shape(patches))
+    if np.shape(patches_mean_var)[1]!=2*input_shape[0]: # This condition checks for 39CC
+        if np.shape(patches_mean_var)[1]==44:
+            patches_mean_var = patches_mean_var[:,list(range(0,21))+list(range(22,43))]
+        elif np.shape(patches_mean_var)[1]==78:
+            first_7_cep_dim = np.array(list(range(0,7))+list(range(13,20))+list(range(26,33))+list(range(39,46))+list(range(52,59))+list(range(65,72)))
+            patches_mean_var = patches_mean_var[:, first_7_cep_dim]
+    # print('patches_mean_var: ', np.shape(patches_mean_var))
+    
+    return patches_mean_var
+
+
+
+
+def textgrids2csv(path):
+    path += '/'
+    root, dirs, all_files = next(os.walk(path))
+    textgrid_idx = np.squeeze(np.where([fl.split('.')[-1]=='TextGrid' for fl in all_files]))
+    print(textgrid_idx)
+    files = [all_files[idx] for idx in textgrid_idx]
+    print(files)
+    
+    for fl in files:
+        grid = textgrids.TextGrid(path+fl)
+        clip = grid['clip']
+        print(clip)
+
+        opFile = path + '/' + fl.split('.')[0] + '.csv'
+        fid = open(opFile, 'a+', encoding = 'utf-8')
+        heading = 'label,duration,xmin,xmax'
+        fid.write(heading + '\n')
+        for intval in clip:
+            print(intval.text, intval.dur, intval.xmin, intval.xmax)
+            values = intval.text + ',' + str(intval.dur) + ',' + str(intval.xmin) + ',' + str(intval.xmax)
+            fid.write(values + '\n')    
+        fid.close()
